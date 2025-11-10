c0e8f5c16ebeca351593abbd9282935ed548766d
c-petersen <100168100+c-petersen@users.noreply.github.com>
2025-11-10 13:21:07 -0600

    package check updates

--- check.cmd
+++ check.cmd
@@ -1,8 +1,9 @@
 @echo off
 setlocal
-set path=%path%;"C:\Program Files\R\R-4.5.2\bin\x64";
+::set path=%path%;"C:\Program Files\R\R-4.5.2\bin\x64";
+set path=%path%;"C:\Program Files\R\R-devel\bin\x64";
 set path=%path%;C:\tools\Rtools45;C:\tools\Rtools45\bin;
 ::set path=%path%;"C:\Program Files\MiKTeX 2.9\miktex\bin\x64";
-R CMD build %1
+R CMD check --as-cran %1
 endlocal
 
